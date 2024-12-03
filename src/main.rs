fbb5f32408fc99e1c833d0d55f8573b12ffd3aed
Dave Corley <smokydave@icloud.com>
2024-12-03 13:07:13 -0600

    FEAT: Rewrite everything to use a config toml and support the additional features from our openmw-cfg fork

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,197 @@
-// use std::collections::HashMap;
-// use std::env;
-// use std::fs;
-use std::io;
-
-use palette::{
-    rgb::{Rgb, Srgb},
-    FromColor, Hsv, IntoColor,
+use std::{
+    fs::{create_dir_all, read_to_string, File, OpenOptions},
+    io::{Result, Write},
+    path::{Path, PathBuf},
 };
 
+use openmw_cfg::{
+    config_path as openmw_cfg_dir, get_config as get_openmw_cfg, get_data_local_dir, get_plugins,
+};
+use palette::{rgb::Srgb, FromColor, Hsv, IntoColor};
+use serde::{Deserialize, Serialize};
 use tes3::esp::*;
+use toml::{
+    to_string as toml_to_string, Table,
+    Value::{Boolean, Float},
+};
 
-fn main() -> io::Result<()> {
-    let config = openmw_cfg::get_config().expect("Failed to get config");
-    let plugins = openmw_cfg::get_plugins(&config).expect("Failed to get plugins");
+const CONFIG_SHOULD_ALWAYS_PARSE_ERR: &str =
+    "Config was already loaded and should never fail to parse!";
+const DEFAULT_AUTO_INSTALL: bool = true;
+const DEFAULT_CONFIG_NAME: &str = "lightconfig.toml";
+const DEFAULT_DISABLE_FLICKER: bool = true;
+const DEFAULT_DO_LOG: bool = false;
+const GET_CONFIG_ERR: &str = "Failed to read openmw.cfg from";
+const GET_PLUGINS_ERR: &str = "Failed to read plugins in openmw.cfg from";
+const LOG_NAME: &str = "lightconfig.log";
+const PLUGIN_LOAD_FAILED_ERR: &str = "Failed to load plugin from {}: {}";
+const PLUGIN_NAME: &str = "S3LightFixes.omwaddon";
+const PLUGINS_MUST_EXIST_ERR: &str = "Plugins must exist to be loaded by openmw-cfg crate!";
+const SHIT_GOT_REAL: &str = "Toml library failed! Major dysfuckulation!";
+const STD_DEFAULT_HUE: f32 = 0.6;
+const STD_DEFAULT_SAT: f32 = 0.8;
+const STD_DEFAULT_VAL: f32 = 0.57;
+const STD_DEFAULT_RAD: f32 = 2.0;
+const STD_COLORED_HUE: f32 = 1.0;
+const STD_COLORED_SAT: f32 = 0.9;
+const STD_COLORED_VAL: f32 = 0.7;
+const STD_COLORED_RAD: f32 = 1.1;
 
-    // Values to come from toml config
-    let disable_flickering = true;
+#[derive(Debug, Deserialize, Serialize)]
+struct LightConfig {
+    auto_install: bool,
+    disable_flickering: bool,
+    save_log: bool,
+    standard_hue: f32,
+    standard_saturation: f32,
+    standard_value: f32,
+    standard_radius: f32,
+    colored_hue: f32,
+    colored_saturation: f32,
+    colored_value: f32,
+    colored_radius: f32,
+}
 
-    let standard_hue = 0.62;
-    let standard_saturation = 0.8;
-    let standard_radius = 1.2;
-    let standard_value = 0.57;
+impl Default for LightConfig {
+    fn default() -> LightConfig {
+        LightConfig {
+            auto_install: DEFAULT_AUTO_INSTALL,
+            disable_flickering: DEFAULT_DISABLE_FLICKER,
+            save_log: DEFAULT_DO_LOG,
+            standard_hue: STD_DEFAULT_HUE,
+            standard_saturation: STD_DEFAULT_SAT,
+            standard_value: STD_DEFAULT_VAL,
+            standard_radius: STD_DEFAULT_RAD,
+            colored_hue: STD_COLORED_HUE,
+            colored_saturation: STD_COLORED_SAT,
+            colored_value: STD_COLORED_VAL,
+            colored_radius: STD_COLORED_RAD,
+        }
+    }
+}
 
-    let colored_hue = 1.0;
-    let colored_saturation = 0.9;
-    let colored_value = 0.7;
-    let colored_radius = 1.11;
+fn openmw_config_path() -> String {
+    let config_path = openmw_cfg_dir();
+
+    if config_path
+        .to_string_lossy()
+        .to_ascii_lowercase()
+        .contains("openmw.cfg")
+    {
+        config_path
+            .parent()
+            .unwrap_or(&config_path)
+            .to_string_lossy()
+            .to_string()
+    } else {
+        config_path.to_string_lossy().to_string()
+    }
+}
+
+fn find_light_config(data_local: &String) -> Option<String> {
+    let directories = [
+        std::env::current_exe()
+            .ok()?
+            .to_path_buf()
+            .to_str()?
+            .to_string(),
+        openmw_config_path(),
+        data_local.to_string(),
+    ];
+
+    for dir in directories.iter() {
+        let file_path = std::path::Path::new(dir.as_str()).join(DEFAULT_CONFIG_NAME);
+        if file_path.exists() {
+            if let Ok(content) = read_to_string(&file_path) {
+                return Some(content);
+            }
+        }
+    }
+    None
+}
+
+fn load_light_config(config_data: String) -> LightConfig {
+    if let Ok(mut config_toml) = config_data.parse::<Table>() {
+        LightConfig {
+            auto_install: config_toml
+                .remove("auto_install")
+                .unwrap_or(Boolean(DEFAULT_AUTO_INSTALL))
+                .as_bool()
+                .expect(SHIT_GOT_REAL),
+            disable_flickering: config_toml
+                .remove("disable_flickering")
+                .unwrap_or(Boolean(DEFAULT_DISABLE_FLICKER))
+                .as_bool()
+                .expect(SHIT_GOT_REAL),
+            save_log: config_toml
+                .remove("save_log")
+                .unwrap_or(Boolean(DEFAULT_DO_LOG))
+                .as_bool()
+                .expect(SHIT_GOT_REAL),
+            standard_hue: config_toml
+                .remove("standard_hue")
+                .unwrap_or(Float(STD_DEFAULT_HUE.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            standard_saturation: config_toml
+                .remove("standard_desaturation")
+                .unwrap_or(Float(STD_DEFAULT_SAT.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            standard_value: config_toml
+                .remove("standard_value")
+                .unwrap_or(Float(STD_DEFAULT_VAL.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            standard_radius: config_toml
+                .remove("standard_radius")
+                .unwrap_or(Float(STD_DEFAULT_RAD.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            colored_hue: config_toml
+                .remove("colored_hue")
+                .unwrap_or(Float(STD_COLORED_HUE.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            colored_saturation: config_toml
+                .remove("colored_desaturation")
+                .unwrap_or(Float(STD_COLORED_SAT.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            colored_value: config_toml
+                .remove("colored_value")
+                .unwrap_or(Float(STD_COLORED_VAL.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+            colored_radius: config_toml
+                .remove("colored_radius")
+                .unwrap_or(Float(STD_COLORED_RAD.into()))
+                .as_float()
+                .expect(SHIT_GOT_REAL) as f32,
+        }
+    } else {
+        LightConfig::default()
+    }
+}
+
+fn main() -> Result<()> {
+    let mut config =
+        get_openmw_cfg().expect(&format!("{} {}!", GET_CONFIG_ERR, openmw_config_path()));
+    let plugins =
+        get_plugins(&config).expect(&format!("{} {}!", GET_PLUGINS_ERR, openmw_config_path()));
+    let userdata_dir = get_data_local_dir(&config);
+
+    let light_config = if let Some(light_config) = find_light_config(&userdata_dir) {
+        load_light_config(light_config)
+    } else {
+        let path: PathBuf = Path::new(&openmw_config_path()).join(DEFAULT_CONFIG_NAME);
+        let _ = write!(
+            File::create(path).expect("Failed to create file"),
+            "{}",
+            toml_to_string(&LightConfig::default()).unwrap()
+        );
+        LightConfig::default()
+    };
 
     let mut generated_plugin = Plugin::new();
     let mut used_ids: Vec<String> = Vec::new();
@@ -38,6 +204,10 @@ fn main() -> io::Result<()> {
     };
 
     for plugin_path in plugins.iter().rev() {
+        if plugin_path.to_string_lossy().contains(PLUGIN_NAME) {
+            continue;
+        }
+
         let extension = match plugin_path.extension() {
             Some(extension) => extension,
             None => continue,
@@ -56,11 +226,7 @@ fn main() -> io::Result<()> {
         let mut plugin = match Plugin::from_path(plugin_path) {
             Ok(plugin) => plugin,
             Err(e) => {
-                eprintln!(
-                    "Failed to load plugin from {}: {}",
-                    plugin_path.display(),
-                    e
-                );
+                eprintln!("{} {} {}", PLUGIN_LOAD_FAILED_ERR, plugin_path.display(), e);
                 continue;
             }
         };
@@ -96,7 +262,7 @@ fn main() -> io::Result<()> {
                 continue;
             }
 
-            if disable_flickering {
+            if light_config.disable_flickering {
                 light
                     .data
                     .flags
@@ -120,18 +286,19 @@ fn main() -> io::Result<()> {
             let hue = hsv.hue.into_degrees();
 
             if hue > 64.0 || hue < 14.0 {
-                light.data.radius = (colored_radius * light.data.radius as f32) as u32;
+                light.data.radius = (light_config.colored_radius * light.data.radius as f32) as u32;
                 hsv = Hsv::new(
-                    hue * colored_hue,
-                    hsv.saturation * colored_saturation,
-                    hsv.value * colored_value,
+                    hue * light_config.colored_hue,
+                    hsv.saturation * light_config.colored_saturation,
+                    hsv.value * light_config.colored_value,
                 );
             } else {
-                light.data.radius = (standard_radius * light.data.radius as f32) as u32;
+                light.data.radius =
+                    (light_config.standard_radius * light.data.radius as f32) as u32;
                 hsv = Hsv::new(
-                    hue * standard_hue,
-                    hsv.saturation * standard_saturation,
-                    hsv.value * standard_value,
+                    hue * light_config.standard_hue,
+                    hsv.saturation * light_config.standard_saturation,
+                    hsv.value * light_config.standard_value,
                 );
             }
 
@@ -151,7 +318,7 @@ fn main() -> io::Result<()> {
             let plugin_size = std::fs::metadata(plugin_path)?.len();
             let plugin_string = plugin_path
                 .file_name()
-                .expect("Plugins must exist to be loaded by openmw-cfg crate!")
+                .expect(PLUGINS_MUST_EXIST_ERR)
                 .to_string_lossy()
                 .to_owned()
                 .to_string();
@@ -162,52 +329,36 @@ fn main() -> io::Result<()> {
     generated_plugin.objects.push(TES3Object::Header(header));
     generated_plugin.sort_objects();
 
-    let _ = generated_plugin.save_path("S3LightFixes.omwaddon");
+    let userdata_path = Path::new(&userdata_dir);
+    if !userdata_path.exists() {
+        create_dir_all(userdata_path)?;
+    }
 
-    dbg!(&generated_plugin);
+    let plugin_path = Path::new(&userdata_dir).join(PLUGIN_NAME);
+    let _ = generated_plugin.save_path(plugin_path);
+
+    if light_config.auto_install {
+        let has_lightfixes_iter = config
+            .section_mut::<String>(None)
+            .expect(CONFIG_SHOULD_ALWAYS_PARSE_ERR)
+            .get_all("content")
+            .find(|s| *s == PLUGIN_NAME);
+
+        if let None = has_lightfixes_iter {
+            let config_path = format!("{}/openmw.cfg", openmw_config_path());
+
+            if !read_to_string(&config_path)?.contains(PLUGIN_NAME) {
+                let mut file = OpenOptions::new().append(true).open(&config_path)?;
+                writeln!(file, "{}", format!("content={}", PLUGIN_NAME))?;
+            }
+        }
+    }
+
+    if light_config.save_log {
+        let path: PathBuf = Path::new(&openmw_config_path()).join(LOG_NAME);
+        let mut file = File::create(path)?;
+        let _ = write!(file, "{}", format!("{:#?}", &generated_plugin));
+    }
 
     Ok(())
-}
-
-const NO_HOME: &str = "BAAAAHHH";
-pub fn default_config_path() -> String {
-    if cfg!(target_os = "linux") {
-        format!(
-            "{}/.config/openmw/openmw.cfg",
-            std::env::var("HOME").expect(NO_HOME)
-        )
-    } else if cfg!(target_os = "macos") {
-        format!(
-            "{}/Library/Preferences/openmw/openmw.cfg",
-            std::env::var("HOME").expect(NO_HOME)
-        )
-    } else if cfg!(target_os = "windows") {
-        format!(
-            "C:\\Users\\{}\\Documents\\My Games\\OpenMW\\openmw.cfg",
-            std::env::var("USERNAME").expect(NO_HOME)
-        )
-    } else {
-        panic!("Unsupported operating system");
-    }
-}
-
-pub fn default_userdata_path() -> String {
-    if cfg!(target_os = "linux") {
-        format!(
-            "{}/.local/share/openmw/data/",
-            std::env::var("HOME").expect(NO_HOME)
-        )
-    } else if cfg!(target_os = "macos") {
-        format!(
-            "{}/Library/Application Support/openmw/data/",
-            std::env::var("HOME").expect(NO_HOME)
-        )
-    } else if cfg!(target_os = "windows") {
-        format!(
-            "C:\\Users\\{}\\Documents\\My Games\\OpenMW\\data\\",
-            std::env::var("USERNAME").expect(NO_HOME)
-        )
-    } else {
-        panic!("Unsupported operating system");
-    }
 }
