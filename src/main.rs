4c0517980aadba8378807381b419f3900c821c12
Duron27 <duron27@gmail.com>
2026-09-08 00:17:30 -0400

    Prevent binary build for Android in main.rs
    Add Android-specific main function to prevent binary build.

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,10 @@
 fn main() -> std::io::Result<()> {
     s3lightfixes::run()
 }
+
+// For Android, main.rs won't be compiled as a binary
+#[cfg(target_os = "android")]
+fn main() {
+    // This won't be built as a binary for Android
+    compile_error!("This binary should not be built for Android");
+}
