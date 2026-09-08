00b5cf887dc6576eb4a896794bfdb886b94dd22e
Duron27 <duron27@gmail.com>
2026-09-08 01:55:21 -0400

    Add android target shared library.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-#[cfg(not(target_os = "android"))]
+#![cfg(not(target_os = "android"))]
 fn main() -> std::io::Result<()> {
     s3lightfixes::run()
-}
-
-// For Android, main.rs won't be compiled as a binary
-#[cfg(target_os = "android")]
-fn main() {
-    // This won't be built as a binary for Android
-    compile_error!("This binary should not be built for Android");
 }
